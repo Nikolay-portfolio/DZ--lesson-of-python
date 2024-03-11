de04a6c3c08dbe4856210c42c7ade6738ee0d14d
Николай Дьяконов <147997121+Nikolay-portfolio@users.noreply.github.com>
2024-03-11 11:58:45 +0300

    Delete lesson_1 directory

--- lesson_1/new2.py
+++
@@ -1,5 +0,0 @@
-class Mailing:
-    to_address = 'Address'
-    from_address = 'Address'
-    cost = '000'
-    track = 'строка'
--- lesson_1/smartphone.py
+++
@@ -1,5 +0,0 @@
-class Smartphone:
-    def __init__(self, "brand", "model", "phone_number"):
-        self.b = brand
-        self.m = model
-        self.p = phone_number
