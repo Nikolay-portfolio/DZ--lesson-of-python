f0643c50aea5ae26647f8503ab764bbcc81541b5
Николай Дьяконов <147997121+Nikolay-portfolio@users.noreply.github.com>
2024-03-11 12:29:25 +0300

    Add files via upload

---
+++ lesson_1/lesson1.py
@@ -0,0 +1,18 @@
+
+
+def sum(num_1, num_2):
+    
+    result = num_1+num_2
+    return result
+
+def multiply(x,y):
+    return x*y
+
+
+
+
+x= sum (10000,42)
+print(x)
+
+m = multiply(3,4)
+print(m)
