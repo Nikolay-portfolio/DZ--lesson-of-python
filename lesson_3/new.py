b2cbfd872f9020fa574f72ca8e71aab98cc0d651
Николай Дьяконов <147997121+Nikolay-portfolio@users.noreply.github.com>
2024-03-17 20:35:33 +0300

    Delete lesson_3/new.py

--- lesson_3/new.py
+++
@@ -1,9 +0,0 @@
-class Address:
-    def __init__(self, index, city, street, home, kv)
-        self.index = index
-        self.city = city
-        self.street = street
-        self.home = home
-        self.kv = kv
-
-
