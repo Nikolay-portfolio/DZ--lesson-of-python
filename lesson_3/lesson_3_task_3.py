b375f74fdc80e1da4fb5e9db6af9be03bbe1818f
Николай Дьяконов <147997121+Nikolay-portfolio@users.noreply.github.com>
2024-03-17 20:36:20 +0300

    Add files via upload

---
+++ lesson_3/lesson_3_task_3.py
@@ -0,0 +1,5 @@
+from new import Address
+from new2 import Mailing
+pack = Mailing("to_address", "from_address", "track", "cost" )
+packet = Address("index", "city", "street", "home", "kv")
+pack.ok()
---
+++ lesson_3/new2.py
@@ -0,0 +1,18 @@
+from new import Address
+class Mailing:
+    to_address = 'Address'
+    from_address = 'Address'
+    cost = '000'
+    track = " "
+    def __init__(self, to_address, from_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
+         
+    def ok(self):
+        print("Отправление", self.track, "из" , self.index , self.city , self.street , self.home , self.kv , 
+        "в" ,self.index , self.city , self.street , self.home , self.kv ,"Стоимость " , self.cost , "рублей")
+
+        
+    
