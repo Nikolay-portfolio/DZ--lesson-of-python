e735b6ee7b56100a45574a7bd1a3ff33da8c0948
Николай Дьяконов <147997121+Nikolay-portfolio@users.noreply.github.com>
2024-03-18 19:56:49 +0300

    Update lesson_3_task_3.py

--- lesson_3/lesson_3_task_3.py
+++ lesson_3/lesson_3_task_3.py
@@ -2,4 +2,4 @@ from new import Address
 from new2 import Mailing
 pack = Mailing("to_address", "from_address", "track", "cost" )
 packet = Address("index", "city", "street", "home", "kv")
-pack.ok()
+pack.letter()
